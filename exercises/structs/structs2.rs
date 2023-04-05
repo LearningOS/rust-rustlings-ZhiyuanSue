360fa231d62ac1b3254c7b62a42970a6b0b5e1b1
ZhiyuanSue <2262387848@qq.com>
2023-04-05 19:34:29 +0800

    update

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -37,6 +37,7 @@ mod tests {
         // TODO: Create your own order using the update syntax and template above!
         let your_order = Order{
             name:String::from("Hacker in Rust"),
+            count:1,
             ..order_template
         };
         assert_eq!(your_order.name, "Hacker in Rust");
